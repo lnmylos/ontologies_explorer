0c597aabf928e559435ee3395ccdbfda4db8bd37
Goldensteev <fletcher.steven@hotmail.fr>
2024-07-20 12:23:13 +0200

    lint tests

--- tests/test_fhir_app.py
+++ tests/test_fhir_app.py
@@ -1,24 +1,21 @@
-"""Test for the FHIR API functions """
+"""Test for the FHIR API functions."""
+
+from pathlib import Path
+
 import yaml
 
 from utils.fhir_api import build_url, load_config
 
 
 def test_load_config(tmp_path) -> None:
-    """
-    Test for the load_config function. Creates a mock config file and test if
-    the data can be loaded.
-
-    Args:
-        tmp_path (str): _description_
-    """
+    """Test for the load_config function."""
     config_data = {
         "fhir_api_base": "https://github.com/",
         "endpoints": {"search": "octocat?tab=following"},
     }
-    config_file = tmp_path / "settings.yml"
+    config_file = Path(tmp_path / "settings.yml")
 
-    with open(config_file, "w", encoding="utf-8") as file_:
+    with Path(config_file).open("w", encoding="utf-8") as file_:
         yaml.dump(config_data, file_)
 
     loaded_config = load_config(config_file_path=config_file)
@@ -27,13 +24,7 @@ def test_load_config(tmp_path) -> None:
 
 
 def test_build_url() -> None:
-    """
-    Test for the build_url function. Creates a mock config file and test that the function
-    returns a valid url.
-
-    Args:
-        tmp_path (_type_): _description_
-    """
+    """Test for the build_url function."""
     config_data = {
         "fhir_api_base": "https://github.com/",
         "endpoints": {"search": "octocat?tab=following"},
